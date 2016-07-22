14da715d277ec553c269718c2eaf7512d0b43f1e
Kimmo Huoman <kipenroskaposti@gmail.com>
2016-07-22 10:22:44 +0300

    Rename reload to reload_modules to prevent mixup with python's built-in reload.

--- pyfibot/plugins/reload.py
+++ pyfibot/plugins/reload.py
@@ -1,8 +1,8 @@
 from pyfibot.decorators import admin_command
 
 
-@admin_command('reload')
-def reload(bot, sender, message, message_arguments):
+@admin_command('reload_modules')
+def reload_modules(bot, sender, message, message_arguments):
     ''' Reload bot modules. Only for admins. '''
     bot.load_plugins()
     bot.respond('Modules reloaded.', message_arguments)
