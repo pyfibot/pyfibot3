24b03a46eea5cae3a07561187f16ed748543bdbc
Kimmo Huoman <kipenroskaposti@gmail.com>
2016-07-22 22:57:16 +0300

    Combine all (temporary) test plugins to single plugin.

--- pyfibot/plugins/available/test_plugins.py
+++ pyfibot/plugins/available/test_plugins.py
@@ -1,3 +1,4 @@
+import time
 import random
 import string
 from pyfibot.decorators import admin_command
@@ -11,9 +12,24 @@ def long(bot, sender, message, message_arguments):
 
     split_response = []
     split_locations = [10, 20, 40, 80, 100, 200, 300, 400, 500, 600]
+
     for i in range(len(split_locations) - 1):
         if split_locations[i] > len(response):
             break
 
         split_response.append(response[split_locations[i]:split_locations[i + 1]])
+
     bot.respond(' '.join(split_response), message_arguments)
+
+
+@admin_command('sleep')
+def sleep(bot, sender, message, message_arguments):
+    ''' Test function to test plugins hanging. To be removed. '''
+    try:
+        sleep_time = int(message)
+    except:
+        return
+
+    bot.respond('Sleeping for %i seconds.' % sleep_time, message_arguments)
+    time.sleep(sleep_time)
+    bot.respond('Slept for %i seconds.' % sleep_time, message_arguments)
